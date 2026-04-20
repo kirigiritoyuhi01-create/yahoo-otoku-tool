6612df9a117266e9d03041a7e6db5056214637fa
kirigiritoyuhi01-create <kirigiritoyuhi01@gmail.com>
2026-04-20 17:14:10 +0900

    Update fetch_items.py

--- fetch_items.py
+++ fetch_items.py
@@ -1,6 +1,5 @@
 import os
-import json
-import urllib.request
+import requests
 
 def fetch_items(query):
     url = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
@@ -9,7 +8,5 @@ def fetch_items(query):
         "query": query,
         "results": 10
     }
-    req = urllib.request.Request(url, headers={"Content-Type": "application/json"}, data=json.dumps(params).encode("utf-8"))
-    response = urllib.request.urlopen(req)
-    data = json.loads(response.read().decode("utf-8"))
-    return data["hits"]
+    response = requests.get(url, params=params)
+    return response.json()["hits"]
